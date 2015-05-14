9e78571e1892982baf7f7d8b74fd9c29391fde50
surbhi-singh <surbhi.chauhan011@gmail.com>
2015-05-15 02:52:17 +0530

    Create linked lists.py

---
+++ linked lists.py
@@ -0,0 +1,27 @@
+class node_peer(object):
+    def __init__(self, hostname, port_number, next = None):
+        self.hostname = hostname
+        self.port_number = port_number
+        self.next = next
+        
+        
+        
+class linked_list_peer(object):
+    def __init(self, head_peer = None):
+        self.head_peer = head_peer
+        
+        
+        
+class linked_list_RFC(object):
+    def __init(self, head_RFC = None):
+        self.head_RFC = head_RFC
+        
+        
+        
+class node_RFC(object):
+    def __init__(self, RFC_number, title, hostname, next= None):
+        self.RFC_number = RFC_number
+        self.title = title
+        self.hostname = hostname
+        self.next = next
+        
