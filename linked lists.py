56c0761ac9c85c4f157e4c5b9fefa307a860b8dd
surbhi-singh <surbhi.chauhan011@gmail.com>
2015-05-15 16:56:01 +0530

    Update linked lists.py

--- linked lists.py
+++ linked lists.py
@@ -3,17 +3,23 @@ class node_peer(object):
         self.hostname = hostname
         self.port_number = port_number
         self.next = next
+    def get_nextPeer(self):
+        return self.next
+    def set_nextPeer(self, next):
+        self.next = next
+    def get_hostnamePeer(self):
+        return self.hostname
         
         
         
 class linked_list_peer(object):
-    def __init(self, head_peer = None):
+    def __init__(self, head_peer = None):
         self.head_peer = head_peer
         
         
         
 class linked_list_RFC(object):
-    def __init(self, head_RFC = None):
+    def __init__(self, head_RFC = None):
         self.head_RFC = head_RFC
         
         
@@ -24,4 +30,10 @@ class node_RFC(object):
         self.title = title
         self.hostname = hostname
         self.next = next
-        
+    def set_nextRFC(self, next):
+        self.next = next
+    def get_nextRFC(self):
+        return self.next
+    def get_RFC_number(self):
+        return self.RFC_number
+    
