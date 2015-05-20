d2dd6cd41f81a66a8eb29a65207d75f044742a7d
surbhi-singh <surbhi.chauhan011@gmail.com>
2015-05-21 01:29:59 +0530

    Update sever.py

--- sever.py
+++ sever.py
@@ -1,4 +1,14 @@
-import socket
-
-s = socket() //create a new socket in the server
-s.connect()
+from socket import *
+import os
+serverPort = 7734
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind('',serverPort)
+serverSocket.listen()
+print 'Server is up and listening'
+while 1:
+    connectionSocket, clientaddress = serverSocket.accept()
+    if os.fork() == 0:
+          serverSocket.close()
+          implement_child(); //to do
+          exit(0)
+    connectionSocket.close()
