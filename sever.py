83359854b3e060f207e28f7303fb60142643fefe
surbhi-singh <surbhi.chauhan011@gmail.com>
2015-05-31 01:59:33 +0530

    Update sever.py

--- sever.py
+++ sever.py
@@ -6,6 +6,7 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind('',serverPort)
 serverSocket.listen(4)
 newpeer = linked_list_peer()
+rfcadder = addRFC()
 print 'Server is up and listening'
 while 1:
     #accept function accepts a connection request from a client and assigns the new connection socket to 
@@ -17,6 +18,18 @@ while 1:
     newpeer.add_peer(clientname, clientport)
     if os.fork() == 0:
           serverSocket.close()
-          implement_child(); #to do..dnt forget to delete the peer's node from the linked list after the work is done.
+          implement_child(connectionSocket); #to do..dnt forget to delete the peer's node from the linked list after the work is done.
+          newpeer.del_peer(clientname)
           exit(0)
     connectionSocket.close()
+def implement_child(connectionSocket):
+    result = connectionSocket.recv(1024)
+    for line in result.split("\n"):
+        if "ADD" in line:
+            #Do ADD functionality
+            for word in result.split(" "):
+                
+        else if "LIST" in line:
+            #Do LIST functionality
+        else:
+            #Do LOOKUP functionality
