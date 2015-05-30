a08107a223b6c5f0296f250a245c864acf23c479
surbhi-singh <surbhi.chauhan011@gmail.com>
2015-05-30 23:17:06 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,13 @@
 from socket import *
 servername = 'name'
-serverport = 7344
+serverport = 7734
 clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect(servername, serverport)
+
+//ask for input from the user
+
+
+clientSocket.send(sentence)
+result = clientSocket.recv(1024)
+//open connection with the peer.. using the data in result variable
+clientSocket.close()
