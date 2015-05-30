b6179c3eaea19421ca4a2a1600176e75a424f51b
surbhi-singh <surbhi.chauhan011@gmail.com>
2015-05-30 23:56:09 +0530

    Update sever.py

--- sever.py
+++ sever.py
@@ -3,10 +3,15 @@ import os
 serverPort = 7734
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind('',serverPort)
-serverSocket.listen()
+serverSocket.listen(4)
 print 'Server is up and listening'
 while 1:
-    connectionSocket, clientaddress = serverSocket.accept()
+    //accept function accepts a connection request from a client and assigns the new connection socket to connectionSocket
+    // and assigns the ip and port of the lient to the resp. variables
+    connectionSocket, (clientip, clientport) = serverSocket.accept()
+    //getting client's name from its ip
+    clientname = connectionSocket.gethostbyaddr(clientip)
+    
     if os.fork() == 0:
           serverSocket.close()
           implement_child(); //to do
